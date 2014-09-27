418075c28581ae8fa4ce4c3e7695d7a4a14ab946
bigbaddevil7 <st8ples@yahoo.com>
2014-09-27 01:26:09 -0500

    test commit

---
+++ src/main/java/com/bigbaddevil7/rustic/Rustic.java
@@ -0,0 +1,12 @@
+package com.bigbaddevil7.rustic;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * Created by bigbaddevil7 on 9/27/2014.
+ */
+
+@Mod(modid="Rustic", name = "Rustic", version = "1.7.2-0.0.1") //Tells Forge that this is a mod
+public class Rustic {
+
+}
